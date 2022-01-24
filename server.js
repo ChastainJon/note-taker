6a4c19837524397481eb2968055dd9b899d0f019
Jon Chastain <chastain.r.jon@gmail.com>
2022-01-23 21:48:08 -0500

    add api routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const { notes } = require('./db/db')
+const fs = require('fs')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/notes.html'))
@@ -14,6 +16,26 @@ app.get('/api/notes', (req,res) =>{
     let results = notes
     res.json(results)
 })
+app.post('/api/notes', (req, res) => {
+    const note = {
+        title: req.body.title,
+        text: req.body.text,
+        id: notes.length.toString()
+    }
+    notes.push(note)
+    const db = `
+    {
+        "notes":${JSON.stringify(notes)}
+    }
+    `
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        db
+    )
+    
+    
+    
+})  
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
